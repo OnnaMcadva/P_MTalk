e5eb830e07228f73a5bedc1bbd7b3535b7511840
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-11-25 20:18:25 +0000

    -h

--- incs/minitalk.h
+++ incs/minitalk.h
@@ -8,19 +8,28 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
+#include <stddef.h>
 
 #define S_WLC "Welcome to the server!"
 #define S_PID "Server PID: %d\n"
 #define S_CHEV "Received end of transmission\n"
 
+# define C_ERROR "\n\033[0;91mAchtung Achtung !\033[0m"
+# define C_ARGS "\033[38;5;214mAdd the server PID and a string или котика:)))\033[0m"
+# define C_EX_ARGS "\033[0;92mExample : ./client 4242 \"To write a message use double quotation marks\"\033[0m\n"
+
+int ft_atoi_v(const char *str);
+void ft_send_bit(int server_pid, wchar_t c);
+
 void ft_handle_signal(int sig, siginfo_t *siginfo, void *context);
 void ft_print_error(const char *error_message);
 void ft_print_args_error(void);
-void ft_send_bit(int pid, char c, int bit_delay);
+
 void ft_send_message(int server_pid, char *message);
-void ft_putendl_fd(const char *str, int fd);
-int ft_is_str_digit(char *s);
-int ft_atoi(const char *str);
+void ft_putendl_fd_v(const char *str, int fd);
+
+
 
 
 
--- srcs/client.c
+++ srcs/client.c
@@ -2,27 +2,80 @@
 
 #include "../incs/minitalk.h"
 
+int	ft_atoi_v(const char *str)
+{
+	int	result;
+
+    if (str == NULL)
+	{
+        return (0);
+	}
+	result = 0;
+	while (*str == ' ' || *str == '-' || *str == '+' || (9 <= *str && *str <= 13))
+	{
+		str++;
+	}
+	while ('0' <= *str && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result);
+}
+
+void ft_send_bit(int server_pid, wchar_t c)
+{
+    size_t index_bit;
+
+    index_bit = 0;
+    while (index_bit < sizeof(wchar_t) * 8) {
+        if (c & (1ULL << index_bit))
+            kill(server_pid, SIGUSR1);
+        else
+            kill(server_pid, SIGUSR2);
+        usleep(500);
+        index_bit++;
+    }
+}
+
+void	ft_putendl_fd_v(const char *s, int fd)
+{
+	size_t	i;
+
+	if (!s)
+	{
+		return ;
+	}
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+
 int main(int ac, char **av)
 {
     int	i;
     int	server_pid;
 
     i = 0;
-    server_pid = ft_atoi(av[1]);
-    if (ac == 3 && ft_is_str_digit(av[1]) && server_pid > 1)
-    {
-        while (av[2][i])
-        {
-            ft_send_bit(server_pid, av[2][i], 200);
-            i++;
-        }
-        ft_send_bit(server_pid, 10, 200);
-    }
-    else
-    {
-        ft_print_error("Error");
-        ft_print_args_error();
-        return (1);
-    }
-    return (0);
+    server_pid = ft_atoi_v(av[1]);
+    if (ac == 3 && server_pid > 1)
+	{
+		while (av[2][i])
+		{
+			ft_send_bit(server_pid, av[2][i]);
+			i++;
+		}
+		ft_send_bit(server_pid, 10);
+	}
+	else
+	{
+		ft_putendl_fd_v(C_ERROR, 1);
+		ft_putendl_fd_v(C_ARGS, 1);
+		ft_putendl_fd_v(C_EX_ARGS, 1);
+	}
+	return (0);
 }
