9bf5f408e4edbe8825224f795447e3f470a11425
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-11-26 22:02:41 +0100

    Update server.c

--- srcs/server.c
+++ srcs/server.c
@@ -1,7 +1,14 @@
-
-
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: annavm <annavm@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/26 19:32:13 by annavm            #+#    #+#             */
+/*   Updated: 2023/11/26 19:32:16 by annavm           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../incs/minitalk.h"
 
@@ -11,7 +18,7 @@ void	ft_handle_signal(int signal, siginfo_t *siginfo, void *cont)
 	static int	i = 0;
 
 	(void)siginfo;
-    (void)cont;
+	(void)cont;
 	if (signal == SIGUSR1)
 		i |= (0x01 << bit);
 	bit++;
@@ -32,10 +39,9 @@ int	main(void)
 
 	pid = getpid();
 	ft_putendl_fd_v(S_WLC, 1);
-    ft_putstr_fd(S_PID, 1);
-    ft_putnbr_fd_v(pid, 1);
+	ft_putstr_fd(S_PID, 1);
+	ft_putnbr_fd_v(pid, 1);
 	ft_putstr_fd(S_END_TRANS, 1);
-
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = ft_handle_signal;
 	sigemptyset(&sa.sa_mask);
