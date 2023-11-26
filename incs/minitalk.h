bfa2fc504af1fdce9d14057b8e6630cca8b45785
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-11-26 15:21:01 +0100

    Update minitalk.h

--- incs/minitalk.h
+++ incs/minitalk.h
@@ -1,5 +1,7 @@
 
 
+
+
 #ifndef MINITALK_H
 #define MINITALK_H
 
@@ -12,10 +14,10 @@
 #include <stddef.h>
 
 #define S_WLC "\n\033[0;94mWelcome to the server!\033[0m"
-#define S_PID "\033[0;96mServer PID: %d\033[0m\n"
-#define S_END_TRANS "\033[0;90mReceived end of transmission\033[0m\n"
+#define S_PID "\033[0;96mServer PID: \033[0m"
+#define S_END_TRANS "\033[0;90m\nА котик где?..\033[0m\n"
 
-#define C_ERROR "\n\033[0;91mAchtung Achtung !\033[0m"
+#define C_ERROR "\n\033[6;91mAchtung Achtung !\033[0m"
 #define C_ARGS "\033[38;5;214mAdd the server PID and a string или котика:)))\033[0m"
 #define C_EX_ARGS "\033[0;92mExample : ./client 4242 \"To write a message use double quotation marks\"\033[0m\n"
 
@@ -24,11 +26,11 @@ int ft_atoi_v(const char *str);
 void ft_send_bit(int server_pid, char c);
 void	ft_putendl_fd_v(const char *s, int fd);
 
-size_t ft_wstrlen(const wchar_t *wstr);
 void ft_handle_signal(int signal, siginfo_t *siginfo, void *cont);
 
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd_v(int n, int fd);
 
 
 #endif
